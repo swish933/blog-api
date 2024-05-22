37c10a33e9839dc7f1848f381e79dd39472cde4f
Ikem Nomso <ikemnomso@gmail.com>
2024-05-22 16:32:27 +0100

    refactor(publish blog): invalidate cache helper

--- util/helpers.js
+++ util/helpers.js
@@ -1,22 +1,12 @@
 const redisClient = require("../integrations/redis");
 
 const scanAndDelete = async (pattern, count = 100) => {
-	let cursor = 0;
-	let found = [];
-	console.log(pattern);
-	do {
-		const reply = await redisClient.scan(cursor, {
-			MATCH: pattern,
-			COUNT: count,
-		});
+	const iteratorConfig = {
+		MATCH: pattern,
+		COUNT: count,
+	};
 
-		cursor = reply.cursor;
-		found = [...reply.keys];
-	} while (cursor !== 0);
-
-	console.log(found);
-
-	for (const key of found) {
+	for await (const key of redisClient.scanIterator(iteratorConfig)) {
 		await redisClient.del(key);
 	}
 };
